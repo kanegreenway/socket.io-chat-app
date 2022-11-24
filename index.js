5722878462efccd8bff57bbb7af8ac8460fefc8d
kanegreenway <kane.greenway@moochcreative.co.uk>
2022-11-24 14:27:22 +0000

    Added personalities to the avatars

--- index.js
+++ index.js
@@ -7,37 +7,42 @@ const io = require("socket.io")(http);
 var players = {};
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log(`User ${socket.id} connected`);
-  players[socket.id] = { id: socket.id, objectColour: 0x6b6b6b, objectLoc: {}, objectRot: {} };
+    console.log(`User ${socket.id} connected`);
+    players[socket.id] = { id: socket.id, avatarColour: 0x6b6b6b, avatarPosition: {}, avatarRotation: {} };
 
-  socket.on("disconnect", (reason) => {
-    console.log(`User ${socket.id} disconnected`);
-    delete players[socket.id];
-  });
+    socket.on("disconnect", (reason) => {
+        console.log(`User ${socket.id} disconnected`);
+        delete players[socket.id];
+    });
 
-  // Main script //
-  socket.on("sendPositionData", (data) => {
-    players[socket.id].objectLoc = data;
-  });
+    // Main script //
+    socket.on("sendClientData", (playerAvatarObjectPosition, playerCameraObjectRotation, playerAvatarColour) => {
+        players[socket.id].avatarPosition = playerAvatarObjectPosition;
+        players[socket.id].avatarRotation = playerCameraObjectRotation;
+        players[socket.id].avatarColour = playerAvatarColour;
+    });
 
-  socket.on("sendRotationData", (data2) => {
-    players[socket.id].objectRot = data2;
-  });
+    socket.on("sendPositionData", (data) => {
+        players[socket.id].objectLoc = data;
+    });
 
-  socket.on("sendColourData", (data3) => {
-    players[socket.id].objectColour = data3;
-  });
-  
+    socket.on("sendRotationData", (data2) => {
+        players[socket.id].objectRot = data2;
+    });
+
+    socket.on("sendColourData", (data3) => {
+        players[socket.id].objectColour = data3;
+    });
 });
 
 setInterval(() => {
-  io.emit("playerdata", players);
+    io.emit("playerdata", players);
 }, 10);
 
 http.listen(port, () => {
-  console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
